69fa37f22a6672e99cf57b9d5610efb219a7b39f
John Cho <john.c.cho@oracle.com>
2016-01-19 11:19:28 -0500

    updated home js

--- OracleJETApplication/js/modules/home.js
+++ OracleJETApplication/js/modules/home.js
@@ -1,9 +1,122 @@
-define(['ojs/ojcore' ,'knockout'
-   ], function(oj, ko) {
-            function homeContentViewModel() {
-                var self = this;
-                self.something = ko.observable("Hello World");
+define(['knockout', 'ojs/ojcore', 'ojs/ojknockout', 'ojs/ojrouter', 'ojs/ojnavigationlist', 'ojs/ojdatacollection-common', 'ojs/ojdialog',  'ojs/ojtabs', 'ojs/ojconveyorbelt','ojs/ojaccordion', 'ojs/ojcollapsible', 'ojs/ojradioset'
+], function (ko, oj) {
+
+        function homeContentViewModel() {
+        var self = this;
+        
+        //KNOCK detect for lgQuery to return true or false
+        var mdQuery = oj.ResponsiveUtils.getFrameworkQuery(oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.MD_UP);
+        self.medium = oj.ResponsiveKnockoutUtils.createMediaQueryObservable(mdQuery);
+        
+        this.sectionName = ko.observable('Home');
+        this.iconClass = ko.observable('fa fa-home');
+        
+        var Tabs = function(data) {
+            var d = data || {};
+            this.id = ko.observable(d.Id || 0);
+            this.title = ko.observable(d.title || '');
+        };
+        
+        var appSideNavData = [
+            {
+                name: 'Charts',
+                id: 'charts',
+                url: '#'
+            },
+            {
+                name: 'Reports',
+                id: 'reports',
+                url: '#'
+            },
+            {
+                name: 'Links',
+                id: 'links',
+                url: '#'
+            },
+            {
+                name: 'iCare',
+                id: 'icare',
+                url: '#'
+            },
+            {
+                name: 'MyInventory',
+                id: 'myinventory',
+                url: '#'
+            },
+            {
+                name: 'Forecasting',
+                id: 'forecasting',
+                url: '#'
+            },
+            {
+                name: 'Dashboards',
+                id: 'dashboards',
+                url: '#'
+            },
+            {
+                name: 'myLabor',
+                id: 'mylabor',
+                url: '#'
+            },
+            {
+                name: 'Admin',
+                id: 'admin',
+                url: '#'
             }
-            
+
+        ];
+        var appTabData = [
+                    {
+                        name: 'Overview',
+                        id: 'overview',
+                        url: '#'
+                    },
+                    {
+                        name: 'Generic Tab 1',
+                        id: 'inventory-tab-1',
+                        url: '#'
+                    },
+                    {
+                        name: 'Generic Tab 2',
+                        id: 'inventory-tab-2',
+                        url: '#'
+                    },
+                    {
+                        name: 'Generic Tab 3',
+                        id: 'inventory-tab-3',
+                        url: '#'
+                    },
+                    {
+                        name: 'Generic Tab 4',
+                        id: 'inventory-tab-4',
+                        url: '#'
+                    },
+                    {
+                        name: 'Generic Tab 5',
+                        id: 'inventory-tab-5',
+                        url: '#'
+                    }
+
+        ];
+        this.itemOnly = function(context) {
+            return context['leaf'];
+        };
+
+        this.appTabData = ko.observableArray(
+            ko.utils.arrayMap(appTabData, function(c) {return new Tabs(c);})
+        );
+        this.selectedTab = ko.observable();
+
+//        this.selectedItem = ko.observable(selectable: itemOnly);
+
+        self.dataSideSource = new oj.ArrayTableDataSource(appSideNavData, {idAttribute: 'id'});
+        self.dataTabSource = new oj.ArrayTableDataSource(appTabData, {idAttribute: 'id'});
+        self.something = ko.observable("Hello World");
+
+        }
+        //Please Custom JQuery Scripts Here
+
+        
    return homeContentViewModel;
+
 });
