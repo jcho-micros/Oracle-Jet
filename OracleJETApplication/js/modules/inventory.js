80212cba5f1b2f3d50e2ae956db26e9fcc530194
yeperalt <yeperalt@YEPERALT-US.us.oracle.com>
2016-01-19 11:46:08 -0500

    Merge origin/master
    Conflicts:
    	OracleJETApplication/index.html

--- OracleJETApplication/js/modules/inventory.js
+++ OracleJETApplication/js/modules/inventory.js
@@ -3,6 +3,11 @@ define(['ojs/ojcore' ,'knockout'
             function inventoryContentViewModel() {
                 var self = this;
                 self.something = ko.observable("Hello World");
+                $("#buttonOpener").click(function() {
+                    $("#modalDialog1").ojDialog("open"); });
+
+                $("#okButton").click(function() {
+                    $("#modalDialog1").ojDialog("close"); });
             }
             
    return inventoryContentViewModel;
