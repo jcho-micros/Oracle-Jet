d2ca5a3cb9a81e06d5dfc1ba70085d5029588525
yeperalt <yeperalt@YEPERALT-US.us.oracle.com>
2016-01-22 11:56:41 -0500

    new home page

--- OracleJETApplication/js/modules/home.js
+++ OracleJETApplication/js/modules/home.js
@@ -1,131 +1,22 @@
-define(['knockout', 'ojs/ojcore', 'ojs/ojknockout', 'ojs/ojrouter', 'ojs/ojnavigationlist', 'ojs/ojdatacollection-common', 'ojs/ojdialog',  'ojs/ojtabs', 'ojs/ojconveyorbelt','ojs/ojaccordion', 'ojs/ojcollapsible', 'ojs/ojradioset'
+define(['knockout', 'ojs/ojcore'
 ], function (ko, oj) {
-$(function () {       
-    $('a.alert.oj-tabs-title').on('click', '#btnParse', function () { alert('clicked');});
-});
-        function homeContentViewModel() {
+    function homeContentViewModel() {
         var self = this;
-        
-        //KNOCK detect for lgQuery to return true or false
-        var mdQuery = oj.ResponsiveUtils.getFrameworkQuery(oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.MD_UP);
-        self.medium = oj.ResponsiveKnockoutUtils.createMediaQueryObservable(mdQuery);
-        
-        this.sectionName = ko.observable('Home');
-        this.iconClass = ko.observable('fa fa-home');
-        
-        var Tabs = function(data) {
-            var d = data || {};
-            this.id = ko.observable(d.Id || 0);
-            this.title = ko.observable(d.title || '');
-            this.template = ko.observable(d.template || '');
-        };
-        
-//        var appSideNavData = [
-//            {
-//                name: 'Charts',
-//                id: 'charts',
-//                url: '#'
-//            },
-//            {
-//                name: 'Reports',
-//                id: 'reports',
-//                url: '#'
-//            },
-//            {
-//                name: 'Links',
-//                id: 'links',
-//                url: '#'
-//            },
-//            {
-//                name: 'iCare',
-//                id: 'icare',
-//                url: '#'
-//            },
-//            {
-//                name: 'MyInventory',
-//                id: 'myinventory',
-//                url: '#'
-//            },
-//            {
-//                name: 'Forecasting',
-//                id: 'forecasting',
-//                url: '#'
-//            },
-//            {
-//                name: 'Dashboards',
-//                id: 'dashboards',
-//                url: '#'
-//            },
-//            {
-//                name: 'myLabor',
-//                id: 'mylabor',
-//                url: '#'
-//            },
-//            {
-//                name: 'Admin',
-//                id: 'admin',
-//                url: '#'
-//            }
-//
-//        ];
-        var appTabData = [
-            {
-                name: 'Overview',
-                id: 'overview',
-                template: 'tab-overview',
-                url: '#'
-            },
-            {
-                name: 'Generic Tab 1',
-                id: 'inventory-tab-1',
-                template: 'tab-overview',
-                url: '#'
-            },
-            {
-                name: 'Generic Tab 2',
-                id: 'inventory-tab-2',
-                url: '#'
-            },
-            {
-                name: 'Generic Tab 3',
-                id: 'inventory-tab-3',
-                url: '#'
-            },
-            {
-                name: 'Generic Tab 4',
-                id: 'inventory-tab-4',
-                url: '#'
-            },
-            {
-                name: 'Generic Tab 5',
-                id: 'inventory-tab-5',
-                url: '#'
-            }
 
-        ];
-        this.itemOnly = function(context) {
-            return context['leaf'];
-        };
+        self.pageHeading = ko.observable("Home");
+        self.pageHeadingIconClass = ko.observable('fa fa-home');
+        self.organizationName = ko.observable("Micros");
+//        self.level1 = ko.observable("level1");
+//        self.level2 = ko.observable("level2");
+        self.location = ko.observable("North East");
+        self.subLocation = ko.observable("Atlanta");
 
-        this.appTabData = ko.observableArray(
-            ko.utils.arrayMap(appTabData, function(c) {return new Tabs(c);})
-        );
-        this.selectedTab = ko.observable("Overview");
+        self.pageSubNavigation = ko.computed(function () {
+            return self.organizationName() + " | " + self.location() + " | " + self.subLocation();
+        }, self);
 
-        this.selectedItem = ko.observable();
+    }
 
-//        self.dataSideSource = new oj.ArrayTableDataSource(appSideNavData, {idAttribute: 'id'});
-        self.dataTabSource = new oj.ArrayTableDataSource(appTabData, {idAttribute: 'id'});
-        self.something = ko.observable("Hello World");
-
-        }
-        //Please Custom JQuery Scripts Here
-
-
-   
-return homeContentViewModel;
-
-
-
+    return homeContentViewModel;
 
 });
