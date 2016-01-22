0bc9bf34c298f80cc1b158f3f517b102c26448fd
yeperalt <yeperalt@YEPERALT-US.us.oracle.com>
2016-01-22 10:56:19 -0500

    deleted unused files

--- OracleJETApplication/js/modules/labor-tab1.js
+++
@@ -1,9 +0,0 @@
-define(['ojs/ojcore' ,'knockout'
-   ], function(oj, ko) {
-            function homeContentViewModel() {
-                var self = this;
-                self.something = ko.observable("Hello World");
-            }
-            
-   return homeContentViewModel;
-});
--- OracleJETApplication/js/modules/labor-tab2.js
+++
@@ -1,7 +0,0 @@
-define(['knockout', 'ojs/ojcore', 'ojs/ojknockout', 'ojs/ojrouter', 'ojs/ojnavigationlist', 'ojs/ojdatacollection-common', 'ojs/ojdialog',  'ojs/ojtabs', 'ojs/ojconveyorbelt','ojs/ojaccordion', 'ojs/ojcollapsible', 'ojs/ojradioset'
-], function (ko, oj) {
-    function laborTab2ContentViewModel() {
-        
-    }
-   return laborTab2ContentViewModel;
-});
