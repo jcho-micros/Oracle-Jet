5f173e0bddbe314f86f5d6cafb69bed0f8e0aade
yeperalt <yeperalt@YEPERALT-US.us.oracle.com>
2016-01-26 18:05:31 -0500

    employee search mobile update

--- OracleJETApplication/js/modules/partials/employee-search.js
+++ OracleJETApplication/js/modules/partials/employee-search.js
@@ -7,15 +7,24 @@ define([
     'ojs/ojcheckboxset',
     'ojs/ojinputtext',
     'ojs/ojselectcombobox',
-    'ojs/ojradioset', 'promise', 'ojs/ojlistview'
+    'ojs/ojradioset',
+    'promise', 'ojs/ojlistview', 'ojs/ojdatacollection-common', 'ojs/ojmenu'
 ], function (ko, oj) {
 
     function peopleContentViewModel() {
         var self = this;
-        self.buttonClick = function (data, event) {
-            self.clickedButton(event.currentTarget.id);
-            return true;
-        }
+        //current visible state of section, either true or false
+        self.sectionsState = ko.observable(false);
+
+        //Toggles visibility of sections
+        self.toggleSections = function () {
+            self.sectionsState(!self.sectionsState());
+        };
+
+//        self.buttonClick = function (data, event) {
+//            self.clickedButton(event.currentTarget.id);
+//            return true;
+//        }
 
         self._HELP_SOURCE = "http://www.oracle.com";
         self._HELP_DEF = "your custom help definition here";
@@ -25,6 +34,52 @@ define([
         self.val = ko.observableArray(["CH"]);
         //Check Box Observable Default
         self.currentStatus = ko.observable();
+
+        var data = [{
+                firstName: 'Kathryn',
+                lastName: 'Jone',
+                homeStore: 'Home Store',
+                hireDate: '4/15/2015',
+                status: 'Active'
+            },
+            {
+                firstName: 'John',
+                lastName: 'Doe',
+                homeStore: 'Home Store',
+                hireDate: '4/15/2015',
+                status: 'Active'
+            },
+            {
+                firstName: 'Eric',
+                lastName: 'Gordon',
+                homeStore: 'Home Store',
+                hireDate: '4/15/2015',
+                status: 'Active'
+            },
+            {
+                firstName: 'Kathryn',
+                lastName: 'Jone',
+                homeStore: 'Home Store',
+                hireDate: '4/15/2015',
+                status: 'Active'
+            },
+            {
+                firstName: 'Kathryn',
+                lastName: 'Jone',
+                homeStore: 'Home Store',
+                hireDate: '4/15/2015',
+                status: 'Active'
+            }
+               ]
+        self.selectedMenuItem = ko.observable("(None selected yet)");
+
+        self.menuItemSelect = function (event, ui) {
+            var item = $('#listview').ojListView('option', "currentItem");
+            self.selectedMenuItem("Action performed on " + item);
+        };
+        self.dataSource = new oj.ArrayTableDataSource(data, {
+            idAttribute: 'name'
+        });
     }
 
     return peopleContentViewModel;
