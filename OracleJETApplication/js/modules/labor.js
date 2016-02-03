a9cc2ad325f3b39cbf42b188f538c07141ca2cb5
John Cho <john.c.cho@oracle.com>
2016-02-03 12:05:17 -0500

    updated js for labor

--- OracleJETApplication/js/modules/labor.js
+++ OracleJETApplication/js/modules/labor.js
@@ -1,4 +1,16 @@
-define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojrouter', 'ojs/ojnavigationlist', 'ojs/ojconveyorbelt'],
+define([
+    'ojs/ojcore', 
+    'knockout', 
+    'jquery', 
+    'ojs/ojknockout', 
+    'ojs/ojrouter', 
+    'ojs/ojnavigationlist', 
+    'ojs/ojconveyorbelt', 
+    'ojs/ojbutton', 
+    'ojs/ojinputtext',
+    'ojs/ojselectcombobox',
+     'ojs/ojinputnumber',
+    'ojs/ojdialog'],
     function (oj, ko, $) {
         /**
          * The view model for the iCare Child router.
@@ -12,68 +24,85 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojrouter', 'o
 
             self.handleActivated = function (data) {
                 var parentRouter = data.valueAccessor().params;
-
                 this.router = parentRouter.createChildRouter('labortab').configure({
-                    'overview': {
-                        label: 'Overview',
-                        value: 'overview',
-                        isDefault: true
-                    },
-
-                    'employees': {
-                        label: 'Employees',
-                        value: 'employees'
-                    },
-
-                    'schedules-timecards': {
-                        label: 'Schedules & Timecards',
-                        value: 'schedules-timecards'
-                    },
-
-                    'forecasting': {
-                        label: 'Forecasting',
-                        value: 'forecasting'
-                    },
-
-                    'payroll': {
-                        label: 'Payroll',
-                        value: 'payroll'
-                    },
-
-                    'analytics': {
-                        label: 'Analytics',
-                        value: 'analytics'
-                    }
+                    'overview': {label: 'Overview', value: 'overview', isDefault: true},
+                    'employees': {label: 'Employees', value: 'employees'},
+                    'schedules-timecards': {label: 'Schedules & Timecards', value: 'schedules-timecards'},
+                    'forecasting': {label: 'Forecasting', value: 'forecasting'},
+                    'payroll': {label: 'Payroll', value: 'payroll'},
+                    'analytics': {label: 'Analytics', value: 'analytics'}
                 });
+
                 // Now that the router for this view exist, synchronise it with the URL
                 oj.Router.sync();
+
+            };
+            //dialog header information
+            self.filterHeading = ko.observable("Filter");
+            self.filterHeadingIconClass = ko.observable('fa fa-filter');
+            //Dialog location array
+            self.locationList = ko.observableArray([
+               {name: 'atlanta', id:'1', value: 'atlanta', label: 'Atlanta', regional: 'Southeast'},
+               {name: 'washingtondc', id:'2', value: 'washingtondc', label: 'Washington DC', regional: 'East'},
+               {name: 'newyorkcity', id:'3', value: 'newyorkcity', label: 'New York City', regional: 'East'},
+               {name: 'miami', id:'4', value: 'miami',  label: 'Miami', regional: 'Southeast'},
+               {name: 'sanfransico', id:'5', value: 'sanfransico',  label: 'San Fransico', regional: 'West'},
+               {name: 'seattle', id:'6', value: 'seattle', label: 'Seattle', regional: 'West'},
+               {name: 'chicago', id:'7', value: 'chicago', label: 'Chicago', regional: 'Midwest'}
+            ]);
+            self.sortedlocationList = ko.observableArray();
+            
+            self.dataSource = new oj.ArrayTableDataSource(this.locationList, {idAttribute: "id"});
+            self.listLocationCol = ko.observable('atlanta');
+            
+            
+            
+            
+            self.sortLists = function () {
+                self.locationList.sort(function (left, right) {
+                    return left.name == right.name ? 0 : (left.name < right.name ? -1 : 1);
+                });
             }
+//console.log(self.locationList.sort(function (left, right) {
+//                    return left.name == right.name ? 0 : (left.name < right.name ? -1 : 1);
+//                }));
+
+            self.currentValue = ko.observable(0);
+            
             self.selectHandler = function (event, ui) {
                 if ('tabs' === event.target.id && event.originalEvent) {
                     // Invoke go() with the selected item.
                     self.router.go(ui.key);
                 }
-            }
+            };
             self.dispose = function () {
                 this.router.dispose();
                 this.router = null;
-            }
+            };
             self.pageHeading = ko.observable("Labor");
             self.pageHeadingIconClass = ko.observable('fa fa-briefcase');
             self.organizationName = ko.observable("Micros");
             self.region = ko.observable("South East");
             self.location = ko.observable("Atlanta");
-
+            
             self.pageSubNavigation = ko.computed(function() {
                 return self.organizationName() + " | " + self.region() + " | " + self.location();
             }, self);
 
             //current visible state of section, either true or false
             self.sectionsState = ko.observable(false);
-
+            self.resultState = ko.observable(true);
+            
             //Toggles visibility of sections
             self.toggleSections = function () {
                 self.sectionsState(!self.sectionsState());
+            };
+            self.handleOpen =  function() {
+                $("#scrollingDialog").ojDialog("open"); 
+            };
+
+            self.handleOKClose = function() {
+                $("#scrollingDialog").ojDialog("close"); 
             };
 
         };
