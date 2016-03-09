c86820ae3b92c617b4e29adff17167962906473a
John Cho <john.c.cho@oracle.com>
2016-03-08 22:49:33 -0500

    removed reports charts to reports analytics

--- OracleJETApplication/js/modules/reports-charts.js
+++
@@ -1,9 +0,0 @@
-define(['ojs/ojcore' ,'knockout'
-   ], function(oj, ko) {
-            function reportsChartsContentViewModel() {
-                var self = this;
-                self.something = ko.observable("Hello World");
-            }
-
-   return reportsChartsContentViewModel;
-});
