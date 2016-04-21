7f647b0e5082b23a984354950bc8edda629a4ce6
John Cho <john.c.cho@oracle.com>
2016-04-21 09:32:57 -0400

    updated new files and sections

--- OracleJETApplication-V-2/js/viewModels/enterprise.js
+++ OracleJETApplication-V-2/js/viewModels/enterprise.js
@@ -10,10 +10,48 @@ define([
     function homeContentViewModel() {
         var self = this;
 
-        
+        self.sectionsState = ko.observable(false);
         self.pageHeading = ko.observable("Enterprise");
+        //Child Router
+        this.router = undefined;
 
+        self.handleActivated = function (data) {
+            var parentRouter = oj.Router.rootInstance;
+            this.router = parentRouter.createChildRouter('enterprisetabs').configure({
+                'overview': {label: 'Overview', value: 'overview', isDefault: true},
+                'labormanagement': {label: 'Labor Management', value: 'labormanagement'},
+                'inventorymanagement': {label: 'Inventory Management', value: 'inventorymanagement'},
+                'gift-loyalties': {label: 'Gift & Loyalty', value: 'gift-loyalty'},
+                'reportinganalytics': {label: 'Reporting & Analytics', value: 'reportinganalytics'},
+                'locations': {label: 'Location',value: 'locations'},
+                'levels': {label: 'Levels', value: 'levels'},
+                'roles': {label: 'Roles', value: 'roles'},
+                'users': {label: 'Users', value: 'users'}
+            });
+            // Now that the router for this view exist, synchronise it with the URL
+            oj.Router.sync();
+        }
+        self.selectHandler = function (event, ui) {
+            if ('enterpriseTabs' === event.target.id && event.originalEvent) {
+                // Invoke go() with the selected item.
+                self.router.go(ui.key);
+            }
+        }
+        self.dispose = function () {
+            this.router.dispose();
+            this.router = null;
+        }
+        //Toggles visibility of sections
+        self.toggleSections = function () {
+            self.sectionsState(!self.sectionsState());
+            if(self.sectionsState()==true){
+                $('.mobileNavTitleArea .fa-chevron-down').addClass('fa-chevron-up').removeClass('fa-chevron-down');
+            }else if(self.sectionsState()==false){
+                $('.mobileNavTitleArea .fa-chevron-up').addClass('fa-chevron-down').removeClass('fa-chevron-up');
 
+            }
+
+        };
     }
 
     return homeContentViewModel;
--- OracleJETApplication-V-2/js/viewModels/gift-loyalty.js
+++ OracleJETApplication-V-2/js/viewModels/gift-loyalty.js
@@ -10,9 +10,46 @@ define([
     function homeContentViewModel() {
         var self = this;
 
-        
+        self.sectionsState = ko.observable(false);
         self.pageHeading = ko.observable("Gift Loyalty");
+        //Child Router
+        this.router = undefined;
 
+        self.handleActivated = function (data) {
+            var parentRouter = oj.Router.rootInstance;
+
+            self.router = parentRouter.createChildRouter('giftloyaltytab').configure({
+                'profile': {label: 'Profile', value: 'profile', isDefault: true},
+                'schedules-timecards': {label: 'Schedules & Timecards', value: 'schedules-timecards'},
+                'jobs-compensation': {label: 'Jobs & Compensation', value: 'jobs-compensation'},
+                'payroll': {label: 'Payroll', value: 'payroll'},
+                'metrics': {label: 'Metrics', value: 'metrics'},
+                'permissions': {label: 'Permissions', value: 'permissions'}
+            });
+            // Now that the router for this view exist, synchronise it with the URL
+            oj.Router.sync();
+        };
+        self.selectHandler = function (event, ui) {
+            if ('gift-LoyaltyTabs' === event.target.id && event.originalEvent) {
+                // Invoke go() with the selected item.
+                self.router.go(ui.key);
+            }
+        };
+        self.dispose = function () {
+            this.router.dispose();
+            this.router = null;
+        };
+        //Toggles visibility of sections
+        self.toggleSections = function () {
+            self.sectionsState(!self.sectionsState());
+            if(self.sectionsState()==true){
+                $('.mobileNavTitleArea .fa-chevron-down').addClass('fa-chevron-up').removeClass('fa-chevron-down');
+            }else if(self.sectionsState()==false){
+                $('.mobileNavTitleArea .fa-chevron-up').addClass('fa-chevron-down').removeClass('fa-chevron-up');
+
+            }
+
+        };
 
     }
 
